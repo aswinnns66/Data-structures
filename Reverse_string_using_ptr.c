6d4d10810d935c591ba261eee7ccfde6770796cd
Aswin Raj <136782756+aswinnns66@users.noreply.github.com>
2025-05-16 21:59:24 +0530

    Add files via upload

---
+++ Reverse_string_using_ptr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char *s;
+	int len,i;
+	printf("\n Enter the string : ");
+	gets(s;
+	printf("Reverse string : ");
+	len = strlen(s);
+	for(i=len;i>0;i--)
+	{
+		printf("%c",*(s+i));
+	}
+	return 0;
+}
